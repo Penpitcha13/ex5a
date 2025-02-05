0c8e31b28ac1ee5430a0c098f4cd3bf7ed3953ae
Penpitcha <penpitchatong13@gmail.com>
2025-02-05 11:04:16 +0700

    refactor#3 modifying the return message

--- common.js
+++ common.js
@@ -1,4 +1,5 @@
 function validate(pace){
+ 
 	if(isNaN(pace)){
 		msg = "Please verify that the pace you have entered is correct!!!";
 		return {"status":0,"message":msg,"hour":0,"min":0};
@@ -8,25 +9,26 @@ function validate(pace){
 		return {"status":0,"message":msg,"hour":0,"min":0};
 	}
 }
-function paceCal(){
-	var min = dist * pace;
+function paceCal(pace,dist){
+var min = dist * pace;
 	var hour = 0;
 	if(min > 60){
 		hour = Math.floor(min / 60);          
 		min = min % 60;
 	}
-	return {"min":min,"hour":hour};
+	return {"min":min, "hour":hour};
 }
+
 function calculate(p,d){
-		var msg = "";
-		var pace = parseFloat(p);
-		var dist = parseInt(d);
-	//---------------------------
-			var chk = validate(pace);
-			if(chk != null) return chk;
-	//---------------------------
+	var msg = "";
+	var pace = parseFloat(p);
+	var dist = parseInt(d);
+//---------------------------
+var ckk = validate(pace);
+if(ckk != null) return ckk;
+//---------------------------
 	var pc = paceCal(pace,dist);
-	//---------------------------
-		msg = "successfully calculated!"
-		return {"status":1,"message":msg,"hour":hour,"min":min};
+//---------------------------
+	msg = "successfully calculated!"
+	return {"status":1,"message":msg,"hour":pc.hour,"min":pc.min};
 }
